b9a75d8cfb7024a2143932d960cdb5a3a0737b74
ZZ7R2M@ALLYIPOC.COM <ZZ7R2M@ALLYIPOC.COM>
2018-06-21 13:25:24 -0400

    Added FlatRate class

--- src/main/java/com/el/ally/invoicify/models/FlatFeeBillingRecord.java
+++ src/main/java/com/el/ally/invoicify/models/FlatFeeBillingRecord.java
@@ -1,5 +1,30 @@
 package com.el.ally.invoicify.models;
 
+import javax.persistence.Entity;
+
+@Entity
 public class FlatFeeBillingRecord extends BillingRecord {
+
+	private double amount;
+	
+	public FlatFeeBillingRecord() {}
+	
+	public FlatFeeBillingRecord(double amount, String description, Company company) {
+		super(description, company);
+		this.amount = amount;
+	}
+	
+	@Override
+	public double getTotal() {
+		return amount;
+	}
+
+	public double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
 
 }
