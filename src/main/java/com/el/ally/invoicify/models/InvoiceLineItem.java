c689143b6f48dd9ccf9045a62d9c577140855c16
ZZ7R2M@ALLYIPOC.COM <ZZ7R2M@ALLYIPOC.COM>
2018-06-21 13:20:16 -0400

    Added INvoiceLineItem class and RateBase class

--- src/main/java/com/el/ally/invoicify/models/InvoiceLineItem.java
+++ src/main/java/com/el/ally/invoicify/models/InvoiceLineItem.java
@@ -1,5 +1,57 @@
 package com.el.ally.invoicify.models;
 
+import java.sql.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+
+@Entity
 public class InvoiceLineItem {
 
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+
+    @OneToOne
+    private BillingRecord billingRecord;
+    
+    private Date createdOn;
+
+    @ManyToOne
+    private Invoice invoice;    
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public BillingRecord getBillingRecord() {
+        return billingRecord;
+    }
+
+    public void setBillingRecord(BillingRecord billingRecord) {
+        this.billingRecord = billingRecord;
+    }
+
+    public Date getCreatedOn() {
+        return createdOn;
+    }
+
+    public void setCreatedOn(Date createdOn) {
+        this.createdOn = createdOn;
+    }
+
+    public Invoice getInvoice() {
+        return invoice;
+    }
+
+    public void setInvoice(Invoice invoice) {
+        this.invoice = invoice;
+    }
 }
--- src/main/java/com/el/ally/invoicify/models/RateBasedBillingRecord.java
+++ src/main/java/com/el/ally/invoicify/models/RateBasedBillingRecord.java
@@ -1,5 +1,48 @@
 package com.el.ally.invoicify.models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="RateBasedBillingRecord")
 public class RateBasedBillingRecord extends BillingRecord {
+    
+    
+    private double quantity;
+    
+    @Column(length=75, nullable=false)
+    private double rate;
+    
+    
+    
+    public RateBasedBillingRecord(double quantity, double rate, String description, Company company) {
+        super(description, company);
+        this.quantity = quantity;
+        this.rate = rate;
+    }
+
+    @Override
+    public double getTotal() {
+        return rate*quantity;
+    }
+
+    public double getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(double quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getRate() {
+        return rate;
+    }
+
+    public void setRate(double rate) {
+        this.rate = rate;
+    }
+
 
 }
+
