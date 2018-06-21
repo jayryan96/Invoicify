51d8c01118921e16743246565591451779da3df1
ZZ7R2M@ALLYIPOC.COM <ZZ7R2M@ALLYIPOC.COM>
2018-06-21 11:15:12 -0400

    Finished Invoice and Company classes.

--- src/main/java/com/el/ally/invoicify/models/Company.java
+++ src/main/java/com/el/ally/invoicify/models/Company.java
@@ -1,5 +1,60 @@
 package com.el.ally.invoicify.models;
 
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.el.ally.invoicify.models.Company;
+
+@Entity
+@Table(name="company")
 public class Company {
 
+	@Id
+    @GeneratedValue
+    private int id;
+	
+	@Column(length=75, nullable=false)
+	private String name;
+	
+	
+	@OneToMany(mappedBy="company")
+	private List<Invoice> invoices;
+
+
+	public Company(String name, List<Invoice> invoices) {
+		this.name = name;
+		this.invoices = invoices;
+	}
+	
+	public Company() {
+		
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Invoice> getInvoices() {
+		return invoices;
+	}
+
+	public void setInvoices(List<Invoice> invoices) {
+		this.invoices = invoices;
+	}
+
+	public int getId() {
+		return id;
+	}
+	
 }
--- src/main/java/com/el/ally/invoicify/models/Invoice.java
+++ src/main/java/com/el/ally/invoicify/models/Invoice.java
@@ -1,5 +1,91 @@
 package com.el.ally.invoicify.models;
 
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+
+import com.el.ally.invoicify.models.Company;
+
+@Entity
+@Table(name="invoices")
 public class Invoice {
 
+	@Id
+    @GeneratedValue
+    private int id;
+		
+	@ManyToOne
+	private Company company;
+	private Date createdOn;
+	private String invoiceDescription;
+	
+	@OneToMany(mappedBy="invoice", cascade=CascadeType.ALL)
+	private List<InvoiceLineItem> lineItems;
+
+	
+	public Invoice(Company company, Date createdOn, String invoiceDescription) {
+		this.company = company;
+		this.createdOn = createdOn;
+		this.invoiceDescription = invoiceDescription;
+	}
+
+
+	public Invoice() {
+		
+	}
+
+
+	public Company getCompany() {
+		return company;
+	}
+
+
+	public void setCompany(Company company) {
+		this.company = company;
+	}
+
+
+	public Date getCreatedOn() {
+		return createdOn;
+	}
+
+
+	public void setCreatedOn(Date createdOn) {
+		this.createdOn = createdOn;
+	}
+
+
+	public String getInvoiceDescription() {
+		return invoiceDescription;
+	}
+
+
+	public void setInvoiceDescription(String invoiceDescription) {
+		this.invoiceDescription = invoiceDescription;
+	}
+
+
+	public List<InvoiceLineItem> getLineItems() {
+		return lineItems;
+	}
+
+
+	public void setLineItems(List<InvoiceLineItem> lineItems) {
+		this.lineItems = lineItems;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+	
 }
